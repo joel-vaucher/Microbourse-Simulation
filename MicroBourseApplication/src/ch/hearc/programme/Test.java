14a6b445818b810f3b785cb0e1c11d6db5ce0813
Ombang Ndo Charles <charles.ombangndo@he-arc.ch>
2016-04-19 06:41:47 +0200

    Ajout d'un test pour creation d'une table

--- MicroBourseApplication/src/ch/hearc/programme/Test.java
+++ MicroBourseApplication/src/ch/hearc/programme/Test.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.hearc.programme;
 
 import ch.hearc.databasefactory.DataBaseConnection;
 import ch.hearc.exception.DatabaseException;
+import ch.hearc.utilitaires.CreateDataBase;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -20,23 +15,26 @@ import java.sql.Statement;
 public class Test {
     /**
      * @param args the command line arguments
+     * @throws ch.hearc.exception.DatabaseException
      */
     public static void main(String[] args) throws DatabaseException {
         Connection conn;
-        Statement state = null;
+        Statement state;
         try{
             conn = DataBaseConnection.getDataBase().getConnection();
             state = conn.createStatement();           
             
             ResultSet result = state.executeQuery("SELECT * FROM BEBE WHERE ID <= 7");
             while (result.next()) {
-            Long id = result.getLong("ID");
-            String name = result.getString("NOM");
-            System.out.println(id + "\t" + name);
-        }
+                Long id = result.getLong("ID");
+                String name = result.getString("NOM");
+                System.out.println(id + "\t" + name);
+            }
         }catch(SQLException ex){
             ex.getMessage();
         }
+        //Test réussi
+        CreateDataBase creation = new CreateDataBase();
+        creation.createTable();
     }
-    
 }
